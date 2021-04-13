eb1c2891f9ed765987c08038707063ded30f73ea
annie1004619 <annie1004619@naver.com>
2021-04-14 03:17:18 +0900

    add: eslint prettier router설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import CreateVote from './pages/CreateVote';
 
 function App() {
-    return (
-        <div>App</div>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/createvote" component={CreateVote} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
