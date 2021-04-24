6f1000a6218b31e1b9ad0cc30211cfba8d294790
annie1004619 <annie1004619@naver.com>
2021-04-24 16:41:22 +0900

    add: login signup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LoginForm from '../components/login/LoginForm';
+import AuthLayout from '../components/common/AuthLayout';
 
 function Login() {
-  return <div>login</div>;
+  return (
+    <AuthLayout title="로그인" description="학교 이메일로 로그인하세요.">
+      <LoginForm />
+    </AuthLayout>
+  );
 }
-
 export default Login;
