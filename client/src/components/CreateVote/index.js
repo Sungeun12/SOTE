3a8d3d664cae2068f2696834b946b7144f04e5ce
annie1004619 <annie1004619@naver.com>
2021-05-03 03:23:01 +0900

    feat: web3로 vote contract 연결

--- client/src/components/CreateVote/index.js
+++ client/src/components/CreateVote/index.js
@@ -1,55 +1,101 @@
-import React from 'react';
-import {useForm, FormProvider} from "react-hook-form";
-import moment from "moment";
-import styled from 'styled-components'
-import ConnectForm from "./ConnectForm";
-import media from "../../util/style/media";
-import color from "../../util/style/color";
-
-
-
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import { useForm, FormProvider } from 'react-hook-form';
+import styled from 'styled-components';
+import Vote from '../../util/VoteContract/Vote.json';
+import ConnectForm from './ConnectForm';
+import media from '../../util/style/media';
+import color from '../../util/style/color';
 
 function CreateVote() {
-    const methods = useForm({
-        defaultValues: {
-            options:[{name:""},{name:""}]
-        },
-    });
-    const onSubmit = data => {
-        alert('모든 값을 입력해주세요');
-        console.log(data)
-        const startDate = moment(data.startDate).format("YYYY/MM/DD HH:MM a").toString();
-        const endDate = moment(data.endDate).format("YYYY/MM/DD HH:MM a").toString();
-        console.log(startDate)
-        console.log(endDate)
+  const [web3, setWeb3] = useState(new Web3(window.ethereum));
+  const [account, setAccount] = useState('');
+
+  const initWeb3 = async () => {
+    if (window.ethereum) {
+      console.log('recent mode');
+      setWeb3(new Web3(window.ethereum));
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.log('user denied account access...');
+      }
+    } else if (window.web3) {
+      console.log('legacy mode');
+      setWeb3(new Web3(Web3.currentProvider));
+    } else {
+      console.log('none-ethereum browser detected ');
+    }
+  };
+
+  useEffect(() => {
+    async function fetchWeb3() {
+      await initWeb3().then(r => console.log(r));
     }
 
-    return (
-        <FormProvider {...methods}>
-            <Form onSubmit={methods.handleSubmit(onSubmit)}>
-                <ConnectForm/>
-                <ButtonContainer>
-                    <PreviewButton type="button" value="미리보기" />
-                    <SubmitButton type="submit" value="투표 만들기" />
-                </ButtonContainer>
-            </Form>
-        </FormProvider>
+    fetchWeb3();
+  }, []);
 
-    );
+  const methods = useForm({
+    defaultValues: {
+      options: [{ name: '' }, { name: '' }],
+    },
+  });
+
+  const onSubmit = async data => {
+    const optionNames = data.options.map(option => option.name);
+    const selectionType = parseInt(data.selectionType, 2);
+    const openingTime = Math.floor(data.startDate.getTime() / 1000.0);
+    const closingTime = Math.floor(data.endDate.getTime() / 1000.0);
+
+    console.log(account);
+    console.log(typeof startDate);
+    const voteContract = new web3.eth.Contract(Vote.abi);
+
+    voteContract
+      .deploy({
+        data: Vote.bytecode,
+        arguments: [optionNames, selectionType, openingTime, closingTime],
+      })
+      .send({
+        from: account,
+      })
+      .on('transactionHash', hash => {
+        console.log(hash);
+      })
+      .then(newContractInstance => {
+        console.log(newContractInstance.options.address);
+        // contractAddr 변수에 담아 서버에 넘긴다
+      });
+
+    alert('투표가 만들어졌습니다.');
+  };
+  return (
+    <FormProvider {...methods}>
+      <Form onSubmit={methods.handleSubmit(onSubmit)}>
+        <ConnectForm />
+        <ButtonContainer>
+          <PreviewButton type="button" value="미리보기" />
+          <SubmitButton type="submit" value="투표 만들기" />
+        </ButtonContainer>
+      </Form>
+    </FormProvider>
+  );
 }
 
 const Form = styled.form`
   margin: 7vh auto;
   width: 700px;
-    @media (max-width: ${media.tablet}px) {
+  @media (max-width: ${media.tablet}px) {
     width: 80%;
   }
 `;
 const ButtonContainer = styled.div`
-margin: 5vh auto;
-width: 100%;
-display: flex;
-justify-content: center;
+  margin: 5vh auto;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 const SubmitButton = styled.input`
   font-family: 'Nanum Gothic Coding', monospace;
@@ -68,13 +114,13 @@ const SubmitButton = styled.input`
   :hover {
     opacity: 1;
   }
-      @media (max-width: ${media.tablet}px) {
+  @media (max-width: ${media.tablet}px) {
     width: 50%;
   }
 `;
 
- const PreviewButton = styled.input`
- font-family: 'Nanum Gothic Coding', monospace;
+const PreviewButton = styled.input`
+  font-family: 'Nanum Gothic Coding', monospace;
   width: 30%;
   height: 50px;
   text-align: center;
@@ -88,11 +134,10 @@ const SubmitButton = styled.input`
   :hover {
     opacity: 1;
   }
-  margin-right:2vw;
-      @media (max-width: ${media.tablet}px) {
+  margin-right: 2vw;
+  @media (max-width: ${media.tablet}px) {
     width: 50%;
   }
 `;
-
 
 export default CreateVote;
