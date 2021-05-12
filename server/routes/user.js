98b8d5782aef3a87d34f56731884011eeaeeef2b
annie1004619 <annie1004619@naver.com>
2021-05-12 23:57:00 +0900

    fix: 오타

--- server/routes/user.js
+++ server/routes/user.js
@@ -30,7 +30,7 @@ router.post("/sendmail", async function (req, res) {
   User.findOne({ email: req.body.email }, async function (err, user) {
     if (!user) {
       return res.json({
-        loginSucess: false,
+        loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
