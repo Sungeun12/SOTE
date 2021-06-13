2de0189eb5cb19602c5f1ddf8831fb1057c48def
annie1004619 <annie1004619@naver.com>
2021-06-13 18:43:43 +0900

    fix: groupitem

--- client/src/components/group/GroupList/GroupItem.js
+++ client/src/components/group/GroupList/GroupItem.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import { MdPersonOutline } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import * as api from '../../../api/group';
 
 function GroupItem({ description, name, members, image, id }) {
+  const history = useHistory();
+  const join = true;
+  // eslint-disable-next-line no-underscore-dangle
+  const userId = useSelector(state => state.user.user._id);
   console.log(description);
+  const onClick = () => {
+    if (join) {
+      history.push(`/group/${id}/home`);
+    } else {
+      alert('그룹 멤버가 아닙니다.');
+    }
+  };
+  const handleJoin = () => {
+    if (userId) {
+      api.joinGroup(id, userId).then(response => console.log(response));
+    }
+  };
   return (
-    <ItemContainer to={`/group/${id}/home`}>
-      <Img src={`http://localhost:5000/${image}`} alt={image} />
+    <ItemContainer>
+      <Img src={`http://localhost:5000/${image}`} alt={image} onClick={onClick} />
       <TitleWrapper>
-        <Name>{name}</Name>
-        <Button type="button" value="가입" />
+        <Name onClick={onClick}>{name}</Name>
+        <Button type="button" value="가입" onClick={handleJoin} />
       </TitleWrapper>
       <Number>
         <MdPersonOutline size={20} style={{ marginRight: '5px' }} /> {members.length} 명
@@ -19,7 +37,7 @@ function GroupItem({ description, name, members, image, id }) {
   );
 }
 
-const ItemContainer = styled(Link)`
+const ItemContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
