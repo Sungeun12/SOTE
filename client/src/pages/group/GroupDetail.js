5cd84aebab788d6e472b20c5fa4f613462e8e850
annie1004619 <annie1004619@naver.com>
2021-09-09 18:15:29 +0900

    fix: groupdetail내의 router

--- client/src/pages/group/GroupDetail.js
+++ client/src/pages/group/GroupDetail.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { loadIdGroup } from '../../actions/group_actions';
-import Loading from '../../components/common/Loading';
 
 import Home from '../../components/group/GroupDetail/Home';
 import Notice from '../../components/group/GroupDetail/Notice';
@@ -11,22 +9,26 @@ import GroupDetailLayout from '../../components/group/GroupDetail/GroupDetailLay
 import GroupVote from '../../components/group/GroupDetail/GroupVote';
 import Member from '../../components/group/GroupDetail/Member';
 import Manage from '../../components/group/GroupDetail/Manage';
+import NoticeDetail from '../../components/group/GroupDetail/Notice/NoticeDetail/index';
 
 function GroupDetail({ match }) {
   const dispatch = useDispatch();
   const { id } = match.params;
-  const request = useSelector(state => state.group.request);
   useEffect(() => {
     dispatch(loadIdGroup(id));
   }, [dispatch, id]);
 
   return (
     <div>
-      {request && <Loading />}
       <GroupDetailLayout id={id}>
         <Switch>
-          <Route exact path={`/group/${id}/home`} component={Home} />
-          <Route exact path={`/group/${id}/notice`} component={Notice} />
+          <Route exact path={`/group/${id}/home`}>
+            <Home id={id} />
+          </Route>
+          <Route exact path={`/group/${id}/notice`}>
+            <Notice id={id} />
+          </Route>
+          <Route exact path={`/group/${id}/notice/:uid`} component={NoticeDetail} />
           <Route exact path={`/group/${id}/vote`} component={GroupVote} />
           <Route exact path={`/group/${id}/member`} component={Member} />
           <Route exact path={`/group/${id}/manage`} component={Manage} />
