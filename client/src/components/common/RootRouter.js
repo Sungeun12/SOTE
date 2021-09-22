684ade4b7d7e7df184e992a0bdef211d211aa67b
annie1004619 <annie1004619@naver.com>
2021-09-22 13:15:57 +0900

    fix: search와 mypage route 추가

--- client/src/components/common/RootRouter.js
+++ client/src/components/common/RootRouter.js
@@ -12,6 +12,12 @@ import CreateGroup from '../../pages/group/CreateGroup';
 import VoteList from '../../pages/vote/VoteList';
 import VoteDetail from '../../pages/vote/VoteDetail';
 import VoteResult from '../../pages/vote/VoteResult';
+import Profile from '../../pages/mypage/Profile';
+import VoteListMain from '../../pages/vote/VoteListMain';
+import MyVote from '../../pages/mypage/MyVote';
+import MyGroup from '../../pages/mypage/MyGroup';
+import Collections from '../../pages/mypage/Collections';
+import Search from '../../pages/search/Search';
 
 function LoginRoutes() {
   return (
@@ -21,10 +27,16 @@ function LoginRoutes() {
       <Route exact path="/vote/:category/:id" component={VoteDetail} />
       <Route exact path="/vote/:category/:id/closed" component={VoteResult} />
       <Route exact path="/vote/:category" component={VoteList} />
+      <Route exact path="/vote" component={VoteListMain} />
       <Route exact path="/group" component={GroupList} />
       <Route exact path="/group/:id/:tab" component={GroupDetail} />
       <Route exact path="/group/:id/:tab/:uid" component={GroupDetail} />
       <Route exact path="/creategroup" component={CreateGroup} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/myvote/:category" component={MyVote} />
+      <Route exact path="/mygroup" component={MyGroup} />
+      <Route exact path="/collections" component={Collections} />
+      <Route exact path="/search" component={Search} />
       <Route exact path="/signup" component={SignUp} />
     </Switch>
   );
