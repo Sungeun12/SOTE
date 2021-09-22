769a6bcbdb16ff8c36b3bd79d45387425e3c6108
annie1004619 <annie1004619@naver.com>
2021-09-22 13:18:02 +0900

    fix: icon menu location 추가

--- client/src/components/common/Header/IconMenu.js
+++ client/src/components/common/Header/IconMenu.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { BiSearch } from 'react-icons/bi';
 import { MdNotificationsNone } from 'react-icons/md';
 import { RiFileList3Line } from 'react-icons/ri';
 
-function IconMenu() {
+function IconMenu({ location }) {
+  console.log(location.pathname);
+
   return (
     <IconMenuContainer>
       <Link to="/search">
         <BiSearch size="25" color="#484646" />
       </Link>
       <WhiteSpace />
-      <Link to="/myvote">
+      <Link to="/profile">
         <RiFileList3Line size="25" color="#484646" />
       </Link>
       <WhiteSpace />
@@ -32,4 +35,4 @@ const IconMenuContainer = styled.div`
 const WhiteSpace = styled.div`
   width: 0.5rem;
 `;
-export default IconMenu;
+export default withRouter(IconMenu);
